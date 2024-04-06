efa3f93095017be9c3aa4f7947fb7f8a9e1e9c7b
Lowiek Van den Stockt <lowiek@fablabfactory.com>
2024-04-06 22:09:17 +0200

    its aliiive!!!!!!!!

--- test/linux/simple_test/simple_test.c
+++ test/linux/simple_test/simple_test.c
@@ -23,6 +23,23 @@
 #define STATUS_WORD_MASK(x) (x &= 0x6F)
 #define MODES_OF_OPERATION_INDEX (0x6060)
 
+/** Struct for specific outputs **/
+// Outputs (6 bytes (pad to 10)): Controlword (2 bytes) + Target position (4 bytes) + padding (4 bytes)
+typedef struct
+{
+   uint16 controlword;
+   uint32 target_pos;
+} __attribute__((packed)) our_outputs;
+
+/** Struct for specific inputs **/
+// Inputs (10 bytes): postion (4 bytes) + statusword (2 bytes) + error actual value (4 bytes)
+typedef struct
+{
+   uint32 position;
+   uint16 statusword;
+   int32 erroract;
+} __attribute__((packed)) our_inputs;
+
 typedef enum
 {
    control_switch_on = 0,
@@ -58,109 +75,210 @@ volatile int wkc;
 boolean inOP;
 uint8 currentgroup = 0;
 boolean forceByteAlignment = FALSE;
-void set_output_int16(uint16_t slave_nb, uint8_t module_index, int16_t value)
-{
-   uint8_t *data_ptr;
 
-   data_ptr = ec_slave[slave_nb].outputs;
-   /* Move pointer to correct module index*/
-   data_ptr += module_index * 2;
-   /* Read value byte by byte since all targets can't handle misaligned
-         addresses */
-   *data_ptr++ = (value >> 0) & 0xFF;
-   *data_ptr++ = (value >> 8) & 0xFF;
+void set_output_int16(uint16_t slave_nb, uint16_t controlword, uint32_t value)
+{
+   our_outputs *data_ptr;
+
+   data_ptr = (our_outputs *)ec_slave[slave_nb].outputs;
+   data_ptr->controlword = controlword;
+   data_ptr->target_pos = value;
 }
 
-void get_input_int16(uint16_t slave_nb, uint8_t module_index, int16_t *value)
+// Run all startup/init commands (for PREOP->SAFEOP), should return 1
+int configservo(uint16 slave)
 {
-   uint8_t *data_ptr;
+   uint8 u8val;
+   uint16 u16val;
+   uint32 u32val;
+   int retval = 0;
 
-   data_ptr = ec_slave[slave_nb].inputs;
+   // Motor Settings
 
-   // Print all inputs
+   // Set max current
+   u32val = 5000;
+   retval += ec_SDOwrite(slave, 0x8011, 0x11, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8011:11: retval = %d\n", retval);
 
-   // for (int i = 0; i < 8; i++)
-   // {
-   //    printf("Input %d: %d\n", i, data_ptr[i]);
-   // }
+   // Set rated current
+   u32val = 1000;
+   retval += ec_SDOwrite(slave, 0x8011, 0x12, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8011:12: retval = %d\n", retval);
 
-   /* Move pointer to correct module index*/
-   data_ptr += module_index * 2;
-   /* Read value byte by byte since all targets can't handle misaligned
-     addresses */
-   *value |= ((*data_ptr++) & 0xFF);
-   *value |= ((*data_ptr) << 8) & 0xff00;
+   // Set motor pole pairs
+   u8val = 3;
+   retval += ec_SDOwrite(slave, 0x8011, 0x13, FALSE, sizeof(u8val), &u8val, EC_TIMEOUTSAFE);
+   printf("0x8011:13: retval = %d\n", retval);
+
+   // Set torque constant
+   u32val = 100;
+   retval += ec_SDOwrite(slave, 0x8011, 0x16, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8011:16: retval = %d\n", retval);
+
+   // Set rotor moment of intertia
+   u32val = 253;
+   retval += ec_SDOwrite(slave, 0x8011, 0x18, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8011:18: retval = %d\n", retval);
+
+   // Set winding inductance
+   u16val = 70;
+   retval += ec_SDOwrite(slave, 0x8011, 0x19, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x8011:19: retval = %d\n", retval);
+
+   // Set motor speed limitation
+   u32val = 12000;
+   retval += ec_SDOwrite(slave, 0x8011, 0x1b, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8011:1b: retval = %d\n", retval);
+
+   // Set motor temperature warn level
+   u16val = 1200;
+   retval += ec_SDOwrite(slave, 0x8011, 0x2b, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x8011:2b: retval = %d\n", retval);
+
+   // Set motor temperature error level
+   u16val = 1400;
+   retval += ec_SDOwrite(slave, 0x8011, 0x2c, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x8011:2c: retval = %d\n", retval);
+
+   // Set motor thermal time constant
+   u16val = 1050;
+   retval += ec_SDOwrite(slave, 0x8011, 0x2d, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x8011:2d: retval = %d\n", retval);
+
+   // Set winding resistance
+   u32val = 360;
+   retval += ec_SDOwrite(slave, 0x8011, 0x30, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8011:30: retval = %d\n", retval);
+
+   // Set voltage constant
+   u32val = 5500;
+   retval += ec_SDOwrite(slave, 0x8011, 0x31, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8011:31: retval = %d\n", retval);
+
+   // Set commutation offset
+   u16val = 270;
+   retval += ec_SDOwrite(slave, 0x8011, 0x15, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x8011:15: retval = %d\n", retval);
+
+   // Outputs
+
+   // Set torque limitation
+   u16val = 2250;
+   retval += ec_SDOwrite(slave, 0x7010, 0x0b, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x7010:0b: retval = %d\n", retval);
+
+   // Amplifier Settings
+
+   // Set current loop P gain
+   u16val = 39;
+   retval += ec_SDOwrite(slave, 0x8010, 0x13, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x8010:13: retval = %d\n", retval);
+
+   // Set current loop I gain
+   u16val = 19;
+   retval += ec_SDOwrite(slave, 0x8010, 0x12, FALSE, sizeof(u16val), &u16val, EC_TIMEOUTSAFE);
+   printf("0x8010:12: retval = %d\n", retval);
+
+   // Set velocity loop P gain
+   u32val = 224;
+   retval += ec_SDOwrite(slave, 0x8010, 0x15, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8010:15: retval = %d\n", retval);
+
+   // 0x8010:17
+   u32val = 1;
+   retval += ec_SDOwrite(slave, 0x8010, 0x17, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8010:17: retval = %d\n", retval);
+
+   // Set velocity loop I gain
+   u32val = 150;
+   retval += ec_SDOwrite(slave, 0x8010, 0x14, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   printf("0x8010:14: retval = %d\n", retval);
+
+   // FB Settings
+
+   // Set observer feed-forward
+   u8val = 100;
+   retval += ec_SDOwrite(slave, 0x8000, 0x15, FALSE, sizeof(u8val), &u8val, EC_TIMEOUTSAFE);
+   printf("0x8000:15: retval = %d\n", retval);
+
+   // Amplifier Settings
+
+   // Set nominal DC link voltage
+   u32val = 24000;
+   retval += ec_SDOwrite(slave, 0x8010, 0x19, FALSE, sizeof(u32val), &u32val, EC_TIMEOUTSAFE);
+   // TODO: Again, same FPRD command seems to be needed to read in data from the slave
+   // Same for the FPRD messages with Ado = 0x80d and Ado = 0x805
+   printf("0x8010:19: retval = %d\n", retval);
+   return retval;
 }
 
-void * loop_message(void* ptr) {
-   if(!ptr) {
+our_inputs get_input_int16(uint16_t slave_nb)
+{
+   our_inputs *inputs = (our_inputs *)ec_slave[slave_nb].inputs;
+
+   return *inputs;
+}
+
+void *loop_message(void *ptr)
+{
+   if (!ptr)
+   {
       printf("Error: loop_message should have arguments\n");
       pthread_exit(NULL);
    }
 
-   int chk = *(int*)ptr;
+   int chk = *(int *)ptr;
 
    int i, j, oloop, iloop;
 
-            oloop = ec_slave[0].Obytes;
-         if ((oloop == 0) && (ec_slave[0].Obits > 0))
-            oloop = 1;
-         if (oloop > 8)
-            oloop = 8;
-         iloop = ec_slave[0].Ibytes;
-         if ((iloop == 0) && (ec_slave[0].Ibits > 0))
-            iloop = 1;
-         if (iloop > 8)
-            iloop = 8;
+   oloop = ec_slave[0].Obytes;
+   if ((oloop == 0) && (ec_slave[0].Obits > 0))
+      oloop = 1;
+   if (oloop > 8)
+      oloop = 8;
+   iloop = ec_slave[0].Ibytes;
+   if ((iloop == 0) && (ec_slave[0].Ibits > 0))
+      iloop = 1;
+   if (iloop > 8)
+      iloop = 8;
 
    // Start precise timer for 2000us
    // use clock_gettime(CLOCK_MONOTONIC, &ts) for better precision
-   struct timespec tcur={0,0}, tprev={0,0};
+   struct timespec tcur = {0, 0}, tprev = {0, 0};
    clock_gettime(CLOCK_MONOTONIC, &tcur);
    tprev = tcur;
 
-   int target = 0;
+   uint32_t target;
+   our_inputs inputs;
 
-   while(chk > 0) {
+   ec_send_processdata();
+   wkc = ec_receive_processdata(EC_TIMEOUTRET);
+
+   inputs = get_input_int16(1);
+   target = inputs.position + 10000;
+
+   while (chk > 0)
+   {
       i = chk;
       // Start precise timer for 2000us
-      do {
+      do
+      {
          clock_gettime(CLOCK_MONOTONIC, &tcur);
-      } while((tcur.tv_sec - tprev.tv_sec) * 1000000000 + (tcur.tv_nsec - tprev.tv_nsec) < 1000000);
+      } while ((tcur.tv_sec - tprev.tv_sec) * 1000000000 + (tcur.tv_nsec - tprev.tv_nsec) < 1000000);
       tprev = tcur;
 
       // Do logic
       ec_send_processdata();
       wkc = ec_receive_processdata(EC_TIMEOUTRET);
+      inputs = get_input_int16(1);
 
       if (wkc >= expectedWKC)
       {
-         printf("Processdata cycle %4d, WKC %d , O:", i, wkc);
 
-         for(j = 0 ; j < oloop; j++)
-         {
-               printf(" %2.2x", *(ec_slave[0].outputs + j));
-         }
-
-         printf(" I:");
-         for(j = 0 ; j < iloop; j++)
-         {
-               printf(" %2.2x", *(ec_slave[0].inputs + j));
-         }
-         printf(" T:%"PRId64"\r",ec_DCtime);
          needlf = TRUE;
 
-         int16_t statusword = 0;
-         /* The input address for get_input_int16 must be devided by two. 0xA
-         is the starting address of inputs*/
-         get_input_int16(1, (0xC - 0x8) >> 1, (int16_t *)&statusword);
-
-         // statusword = statusword >> 1;
-         // statusword = statusword & ~(0b1 << 5);
-
+         int16_t statusword = inputs.statusword;
          STATUS_WORD_MASK(statusword);
-         // printf("Statusword: %#x (%d)\n", statusword, statusword);
-
 
          switch (statusword)
          {
@@ -175,19 +293,20 @@ void * loop_message(void* ptr) {
             /* Automatic transition (2)*/
             controlword = 0;
             controlword |= (1 << control_enable_voltage) | (1 << control_quick_stop);
-            if(i % 10 == 0) {
+            if (i % 10 == 0)
+            {
                controlword |= 1 << control_fault_reset;
             }
             break;
          }
          case (Ready_to_switch_on):
-         // case (Ready_to_switch_on | (0b1 << 5)):
-         {
-            /* Switch on command for transition (3) */
-            controlword |= 1 << control_switch_on;
-            controlword = 0x07;
-            break;
-         }
+            // case (Ready_to_switch_on | (0b1 << 5)):
+            {
+               /* Switch on command for transition (3) */
+               controlword |= 1 << control_switch_on;
+               controlword = 0x07;
+               break;
+            }
          case (Switch_on):
          {
             /* Enable operation command for transition (4) */
@@ -214,7 +333,7 @@ void * loop_message(void* ptr) {
             uint16_t mode = 8; /* Setting Cyclic Synchronous position */
             int mode_size = sizeof(mode);
             int SDO_result = ec_SDOwrite(1, MODES_OF_OPERATION_INDEX, 0,
-                                       0, mode_size, &mode, EC_TIMEOUTRXM);
+                                         0, mode_size, &mode, EC_TIMEOUTRXM);
             controlword |= 0x1f;
             break;
          }
@@ -241,36 +360,15 @@ void * loop_message(void* ptr) {
          }
          }
 
-         // printf("Controlword: %#x\n", controlword);
-         set_output_int16(1, 0 >> 1, controlword);
+         target = inputs.position + 10000;
 
-         int16_t value_high = 0;
-         int16_t value_low = 0;
-         int32_t value = 0;
+         printf("Target: %d ", target);
+         printf("Value: %d ", inputs.position);
+         printf("Controlword: %#x ", controlword);
+         printf("Statusword: %#x (%d) ", statusword, statusword);
+         printf("Error: %d\r", inputs.erroract);
 
-
-         get_input_int16(1, (0x6 - 0x6) >> 1, (int16_t*) &value_low);
-         get_input_int16(1, (0x8 - 0x6) >> 1, (int16_t*) &value_high);
-         value = ((value_high << 16) & 0xffff0000) | (value_low & 0x00ffff);
-
-         if (target - 5 < value && value < target + 5) {
-            value = target;
-         } else if(value < target) {
-            value += 1000;
-         } else if(value > target) {
-            value -= 1000;
-         }
-
-
-
-         // value = 0x07070707;
-
-         /* Setting output */
-         set_output_int16(1, 0x2 >> 1, (int16_t)(value & 0xffff) );
-         set_output_int16(1, 0x4 >> 1, (int16_t)((value >> 16) & 0xffff));
-
-
-
+         set_output_int16(1, controlword, target);
       }
 
       // Increment chk
@@ -280,7 +378,8 @@ void * loop_message(void* ptr) {
    pthread_exit(NULL);
 }
 
-void start_loop(int chk) {
+void start_loop(int chk)
+{
    // Set attributes for thread to be locked to specific cpu with high (realtime) priority
    pthread_t thread;
    pthread_attr_t attr;
@@ -301,7 +400,6 @@ void start_loop(int chk) {
    pthread_join(thread, NULL);
 }
 
-
 void simpletest(char *ifname)
 {
    int i, j, oloop, iloop, chk;
@@ -318,7 +416,10 @@ void simpletest(char *ifname)
 
       if (ec_config_init(FALSE) > 0)
       {
+
          printf("%d slaves found and configured.\n", ec_slavecount);
+
+         ec_slave[1].PO2SOconfig = configservo;
 
          if (forceByteAlignment)
          {
@@ -336,7 +437,6 @@ void simpletest(char *ifname)
          ec_statecheck(0, EC_STATE_SAFE_OP, EC_TIMEOUTSTATE * 4);
 
          osal_usleep(100000);
-
 
          oloop = ec_slave[0].Obytes;
          if ((oloop == 0) && (ec_slave[0].Obits > 0))
@@ -374,28 +474,21 @@ void simpletest(char *ifname)
 
          osal_usleep(1000000);
 
-
          if (ec_slave[0].state == EC_STATE_OPERATIONAL)
          {
             printf("Operational state reached for all slaves.\n");
             inOP = TRUE;
-
 
             ec_send_processdata();
             wkc = ec_receive_processdata(EC_TIMEOUTRET);
 
             printf("WKC: %d\n", wkc);
 
-            if (wkc>= 1) {
-               int16_t statusword = 0;
-               get_input_int16(1, (0xC - 0x8) >> 1, (int16_t *)&statusword);
-               
-               printf("Statusword: %#x (%d)\n", statusword, statusword);
+            if (wkc >= 1)
+            {
 
-
-            // Start precise loop
-            start_loop(10000);
-
+               // Start precise loop
+               start_loop(10000);
             }
 
             /* cyclic loop */
@@ -431,7 +524,6 @@ void simpletest(char *ifname)
 
             //       STATUS_WORD_MASK(statusword);
             //       printf("Statusword: %#x (%d)\n", statusword, statusword);
-
 
             //       switch (statusword)
             //       {
@@ -518,7 +610,6 @@ void simpletest(char *ifname)
             //       int16_t value_low = 0;
             //       int32_t value = 0;
 
-
             //       get_input_int16(1, (0x6 - 0x6) >> 1, (int16_t*) &value_low);
             //       get_input_int16(1, (0x8 - 0x6) >> 1, (int16_t*) &value_high);
             //       value = ((value_high << 16) & 0xffff0000) | (value_low & 0x00ffff);
@@ -529,8 +620,6 @@ void simpletest(char *ifname)
             //       /* Setting output */
             //       set_output_int16(1, 0x2 >> 1, (int16_t)(value & 0xffff) );
             //       set_output_int16(1, 0x4 >> 1, (int16_t)((value >> 16) & 0xffff));
-
-
 
             //    }
             //    osal_usleep(1000);
